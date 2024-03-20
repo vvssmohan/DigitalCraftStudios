abd0f89c4271adc7b3db080cd26a5fc01ab73c22
vvssmohan <135600699+vvssmohan@users.noreply.github.com>
2024-03-20 14:19:07 +0530

    Create MajorProject.js

---
+++ MajorProject.js
@@ -0,0 +1,22 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    if (name === "" || email === "" || message === "") {
+        alert("Please fill out all fields");
+        event.preventDefault();
+    } else {
+        alert("Thank you! Your message has been sent.");
+    }
+});
